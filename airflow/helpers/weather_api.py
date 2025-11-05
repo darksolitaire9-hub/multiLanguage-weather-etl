ca62f1737bf6a669890fc0964f768ed75bae311f
darksolitaire9 <darksolitaire9@gmail.com>
2025-11-05 15:44:04 +0000

    Initial ETL: weather fetch, helpers, Airflow DAG, data folder structure

--- airflow/helpers/weather_api.py
+++ airflow/helpers/weather_api.py
@@ -1,4 +1,5 @@
 import requests
+from pprint import pprint
 from helpers.geocode_utils import get_city_coordinates
 from helpers.date_utils import get_interval_start_to_end_dates
 from config.constants import CITY_NAME, COUNTRY, WEATHER_API_URL, DAILY_VARIABLES, START_YEAR, TIMEZONE, NUM_YEARS, DIRECTION
@@ -19,12 +20,6 @@ def fetch_weather_data(
         'daily': ','.join(daily_variables),
         'timezone': timezone,
     }
-    response = requests.get(weather_api_url, params=params, timeout=10)
+    response = requests.get(weather_api_url, params=params, timeout=60)
     data = response.json()
-    print(data)
     return data
-
-if __name__ == "__main__":
-    fetch_weather_data(
-        CITY_NAME, COUNTRY, WEATHER_API_URL, start_date, end_date
-    )
